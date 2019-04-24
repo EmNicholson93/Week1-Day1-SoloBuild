8fdffbc2a4feeaab18eb6f2dc90cb6f6130da22b
Eli Nicholson <Eli.m.Nicholson@gmail.com>
2019-04-23 17:46:08 -0700

    succesfully ran 2 more tests

--- test/tests.js
+++ test/tests.js
@@ -1,7 +1,27 @@
 const test = QUnit.test;
 
 function checkResults(rangerAnswer, smokeyAnswer, tybaltAnswer, roryAnswer, kayakAnswer) {
-    let score = 5;
+    let score = 0;
+
+    if(rangerAnswer === 'bengal') {
+        score += 1;
+    }
+
+    if(smokeyAnswer === 'chicks') {
+        score += 1;
+    }
+    
+    if(tybaltAnswer === 'siamese') {
+        score += 1;
+    }
+    
+    if(roryAnswer === 'early') {
+        score += 1;
+    }
+
+    if(kayakAnswer === 'kayaking') {
+        score += 1;
+    }
     return score;
 
 }
@@ -15,7 +35,7 @@ test('return a score of 5 if all answers are correct', assert => {
     const tybaltAnswer = 'siamese';
     const roryAnswer = 'early';
     const kayakAnswer = 'kayaking';
-    const expectedResultOne = '5';
+    const expectedResult = '5';
 
 //act
 
@@ -23,5 +43,45 @@ test('return a score of 5 if all answers are correct', assert => {
 
 //assert
 
-    assert.equal(expectedResultOne, score);
+    assert.equal(expectedResult, score);
+});
+
+test('return a score of 3 if 2 answers are incorrect', assert => {
+
+//arrange
+    
+    const rangerAnswer = 'bengallol';
+    const smokeyAnswer = 'chickssd';
+    const tybaltAnswer = 'siamese';
+    const roryAnswer = 'early';
+    const kayakAnswer = 'kayaking';
+    const expectedResult = '3';
+
+//act
+
+    const score = checkResults(rangerAnswer, smokeyAnswer, tybaltAnswer, roryAnswer, kayakAnswer);
+
+//assert
+
+    assert.equal(expectedResult, score);
+});
+
+test('return a score of 0 if all answers are incorrect', assert => {
+
+//arrange
+    
+    const rangerAnswer = 'bengallol';
+    const smokeyAnswer = 'chickssd';
+    const tybaltAnswer = 'siamesdfe';
+    const roryAnswer = 'earlsy';
+    const kayakAnswer = 'kayasdking';
+    const expectedResult = '0';
+
+//act
+
+    const score = checkResults(rangerAnswer, smokeyAnswer, tybaltAnswer, roryAnswer, kayakAnswer);
+
+//assert
+
+    assert.equal(expectedResult, score);
 });
